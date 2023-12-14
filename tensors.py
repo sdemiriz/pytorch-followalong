1a446d8cc12a2d2f96a15dd106745e43a86b469f
sedatDemiriz <sedatdemiriz97@gmail.com>
2023-12-13 16:50:39 -0800

    feat: wrap up the Tensors chapter of the tutorial

--- tensors.py
+++ tensors.py
@@ -41,4 +41,53 @@ print(f"Shape of tensor: {tensor.shape}")
 print(f"Datatype of tensor: {tensor.dtype}")
 print(f"Device tensor is stored on: {tensor.device}")  # cpu
 
+# A GPU is available
 print(torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    tensor = tensor.to("cuda")
+    print(f"Device tensor is stored on: {tensor.device}")
+
+# Indexing and slicing
+tensor = torch.ones(4, 4)
+tensor[:, 1] = 0
+print(tensor)
+
+# Joining tensors
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# dim=1 is rowwise, dim=2 is columnwise concatenation
+t1 = torch.cat([tensor, tensor, tensor], dim=0)
+print(t1)
+
+# Elementwise multiplication (Hadamard product)
+print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
+print(f"tensor*tensor \n {tensor*tensor} \n")
+
+# Matrix multiplication
+print(f"tensor.matmul(tensor.T) \n {tensor.matmul(tensor.T)} \n")
+print(f"tensor@tensor.T \n {tensor @ tensor.T} \n")
+
+# In-place operations (save on memory but loses history)
+print(tensor, "\n")
+tensor.add_(5)
+print(tensor, "\n")
+
+# Numpy interactions: changing a tensor changes the array as well
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+# Numpy to Tensor
+n = np.ones(5)
+t = torch.from_numpy(n)
+
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
