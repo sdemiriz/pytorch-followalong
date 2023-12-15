93ce7e1fc6b5122d9c3f90090616735afe4c4570
sedatDemiriz <sedatdemiriz97@gmail.com>
2023-12-14 16:45:31 -0800

    feat: add neural network definition

---
+++ nn.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        """Initialize network, inherit from nn.Module"""
+        super(Net, self).__init__()
+
+        # One in channel (image)
+        # Six out channels (feature maps)
+        # Convolution kernel of size 5x5
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+
+        self.fc1 = nn.Linear(in_features=16 * 5 * 5, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.fc3 = nn.Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        """Initialize learning hyperparameters"""
+        # Max pooling over 2x2 window
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+
+        # Flatten all dimensions except batch dimension
+        x = torch.flatten(x, 1)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+
+
+net = Net()
+print(net)
