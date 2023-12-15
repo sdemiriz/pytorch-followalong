5d0f20af2c26a7057ebbd18f8574a9742205cddd
sedatDemiriz <sedatdemiriz97@gmail.com>
2023-12-14 22:23:32 -0800

    feat: define CNN

--- image-classifier.py
+++ image-classifier.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
 
 # ToTensor imports image into a torch Tensor
 # Normalize will normalize each channel RGB to values between -1,1
@@ -47,3 +49,27 @@ classes = (
     "ship",
     "truck",
 )
+
+
+# Define the CNN
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
